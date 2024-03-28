b10ea3c1febb3d321a4b707f87b5577f403b6049
tonylxm <126369686+tonylxm@users.noreply.github.com>
2024-03-28 13:29:42 +1300

    fix redirect

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
-import ReactDOM from "react-dom";
-import App from "./App.tsx";
-import "./index.css";
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-const root = document.getElementById("root");
-ReactDOM.render(<App />, root);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <App />
+)
